cdec701d1b38c09a5b89b6c14e492fd93dd84665
edy4c7 <edy4c7@gmail.com>
2022-03-28 21:03:10 +0900

    initial commit

---
+++ src/main/kotlin/io/github/edy4c7/locationmapper/LocationmapperApplication.kt
@@ -0,0 +1,11 @@
+package io.github.edy4c7.locationmapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocationmapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<LocationmapperApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/edy4c7/locationmapper/LocationmapperApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.edy4c7.locationmapper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocationmapperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
