ecac93530c5311ad9125efde935d11ffb8fdc865
edy4c7 <edy4c7@gmail.com>
2022-07-28 23:32:28 +0900

    Jobの構成を見直し

--- src/main/kotlin/io/github/edy4c7/locationmapper/infrastructures/storage/S3StorageClientImpl.kt
+++ src/main/kotlin/io/github/edy4c7/locationmapper/infrastructures/storage/S3StorageClientImpl.kt
@@ -5,13 +5,17 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.GetUrlRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import java.net.URL
 import java.nio.file.Path
+import kotlin.io.path.name
 
 @Component
 class S3StorageClient(@Value("\${s3.bucket}")private val bucketName:String, private val client: S3Client) : StorageClient {
-    override fun upload(key: String, data: Path) {
-        client.putObject(PutObjectRequest.builder().bucket(bucketName).key(key).build(),
+    override fun upload(key: String, data: Path): URL {
+        client.putObject(PutObjectRequest.builder().bucket(bucketName).key(data.name).build(),
             RequestBody.fromFile(data))
+        return client.utilities().getUrl(GetUrlRequest.builder().bucket(bucketName).key(data.name).build())
     }
 }
