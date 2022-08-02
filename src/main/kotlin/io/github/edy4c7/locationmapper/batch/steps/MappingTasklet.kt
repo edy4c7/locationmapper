1147e2e2a15ccd051ab6d2d8bad3cc29b2b8c46b
edy4c7 <edy4c7@gmail.com>
2022-08-02 22:46:21 +0900

    Serviceクラスを統合

--- src/main/kotlin/io/github/edy4c7/locationmapper/batch/steps/MappingTasklet.kt
+++ src/main/kotlin/io/github/edy4c7/locationmapper/batch/steps/MappingTasklet.kt
@@ -1,6 +1,6 @@
 package io.github.edy4c7.locationmapper.batch.steps
 
-import io.github.edy4c7.locationmapper.domains.services.MappingExecuteService
+import io.github.edy4c7.locationmapper.domains.services.MappingService
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.scope.context.ChunkContext
@@ -12,10 +12,10 @@ import java.io.FileInputStream
 
 @Component
 @StepScope
-class MappingTasklet (
-    @Value("#{jobParameters['mapping.id']}")private val requestId: String,
-    @Value("#{jobParameters['input.file.name']}")private val inputFileName: String,
-    private val service: MappingExecuteService,
+class MappingTasklet(
+    @Value("#{jobParameters['mapping.id']}") private val requestId: String,
+    @Value("#{jobParameters['input.file.name']}") private val inputFileName: String,
+    private val service: MappingService,
 ) : Tasklet {
 
     override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus? {
--- src/main/kotlin/io/github/edy4c7/locationmapper/domains/services/MappingService.kt
+++ src/main/kotlin/io/github/edy4c7/locationmapper/domains/services/MappingService.kt
@@ -1,8 +1,11 @@
 package io.github.edy4c7.locationmapper.domains.services
 
+import io.github.edy4c7.locationmapper.common.utils.unwrap
 import io.github.edy4c7.locationmapper.domains.entities.Mapping
 import io.github.edy4c7.locationmapper.domains.interfaces.storage.StorageClient
+import io.github.edy4c7.locationmapper.domains.mapimagesources.MapImageSource
 import io.github.edy4c7.locationmapper.domains.repositories.MappingRepository
+import io.github.edy4c7.locationmapper.domains.valueobjects.Location
 import org.springframework.batch.core.BatchStatus
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
@@ -10,13 +13,22 @@ import org.springframework.batch.core.explore.JobExplorer
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.io.BufferedReader
 import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.URL
 import java.nio.ByteBuffer
 import java.nio.file.Files
 import java.nio.file.Path
 import java.time.LocalDateTime
 import java.util.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.extension
+import kotlin.io.path.inputStream
 import kotlin.io.path.outputStream
+import kotlin.streams.toList
 
 @Service
 class MappingService(
@@ -24,10 +36,16 @@ class MappingService(
     private val jobExplorer: JobExplorer,
     private val mappingJob: Job,
     private val mappingRepository: MappingRepository,
+    private val mapSource: MapImageSource,
     private val storageClient: StorageClient,
     private val workDir: Path,
     @Value("\${s3.bucket}") private val bucketName: String,
 ) {
+    companion object {
+        const val suffix = ".png"
+        const val FPS = 30
+        const val GPRMC_HEADER = "\$GPRMC"
+    }
 
     fun requestProcess(nmea: InputStream): String {
         val filePath = Files.createTempFile(workDir, "", ".nmea")
@@ -63,6 +81,42 @@ class MappingService(
         return mappingRepository.findById(uuid.toString()).orElse(null)?.let {
             jobExplorer.getJobExecution(it.jobId)?.status
         }
+    }
+
+    fun map(id: String, input: InputStream): URL {
+        val sentences = BufferedReader(InputStreamReader(input)).use { br ->
+            br.lines().filter { it.startsWith(GPRMC_HEADER) }.toList()
+        }
+
+        val digits = (sentences.size * FPS).toString().length
+
+        val output = workDir.resolve("$id.zip")
+
+        ZipOutputStream(output.outputStream()).use { zos ->
+            var count = 0
+            sentences.forEach {
+                val tmp = Files.createTempFile(workDir, "", suffix)
+                val location = Location.fromGprmc(it)
+
+                mapSource.getMapImage(location.latitude, location.longitude).transferTo(tmp.outputStream())
+                for (i in 1..FPS) {
+                    zos.putNextEntry(ZipEntry(String.format("%0${digits}d.${suffix}", count++)))
+                    tmp.inputStream().transferTo(zos)
+                    zos.closeEntry()
+                }
+
+                tmp.deleteIfExists()
+            }
+        }
+
+        val url = storageClient.upload(bucketName, output, "locationmapper.${output.extension}")
+
+        mappingRepository.findById(id).unwrap()?.let {
+            it.uploadedAt = LocalDateTime.now()
+            mappingRepository.save(it)
+        }
+
+        return url
     }
 
     fun expire(retentionDateTime: LocalDateTime) {
