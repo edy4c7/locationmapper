1a2a8e323da76f098570784b82e6665f5fb834bf
edy4c7 <edy4c7@gmail.com>
2022-08-02 21:59:50 +0900

    レスポンスデータ修正

--- src/main/kotlin/io/github/edy4c7/locationmapper/domains/services/MappingService.kt
+++ src/main/kotlin/io/github/edy4c7/locationmapper/domains/services/MappingService.kt
@@ -1,9 +1,9 @@
 package io.github.edy4c7.locationmapper.domains.services
 
-import io.github.edy4c7.locationmapper.domains.dto.JobProgress
 import io.github.edy4c7.locationmapper.domains.entities.Mapping
 import io.github.edy4c7.locationmapper.domains.interfaces.storage.StorageClient
 import io.github.edy4c7.locationmapper.domains.repositories.MappingRepository
+import org.springframework.batch.core.BatchStatus
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.explore.JobExplorer
@@ -57,17 +57,11 @@ class MappingService(
         return Base64.getUrlEncoder().encodeToString(buff.array())
     }
 
-    fun getProgress(id: String) : JobProgress? {
+    fun getProgress(id: String): BatchStatus? {
         val buff = ByteBuffer.wrap(Base64.getUrlDecoder().decode(id))
         val uuid = UUID(buff.long, buff.long)
         return mappingRepository.findById(uuid.toString()).orElse(null)?.let {
-            jobExplorer.getJobExecution(it.jobId)
-        }?.executionContext?.let {
-            JobProgress(
-                id,
-                if (it.containsKey("count.gprmc")) it.getInt("count.gprmc") else null,
-                if (it.containsKey("count.completed")) it.getInt("count.completed") else 0
-            )
+            jobExplorer.getJobExecution(it.jobId)?.status
         }
     }
 
