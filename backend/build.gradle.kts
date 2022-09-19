a10289ec76e0e59496e291e13e8297d27780127f
edy4c7 <edy4c7@gmail.com>
2022-09-19 19:00:36 +0900

    フォルダ移動

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
 	id("org.springframework.boot") version "2.6.5"
@@ -50,4 +51,13 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks {
+	bootJar {
+		archiveBaseName.set("locationmapper")
+	}
+	jar {
+		enabled = false
+	}
 }
