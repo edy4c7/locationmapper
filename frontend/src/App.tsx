59073151bc94c46a289a18008fe066aa0bfe392a
edy4c7 <edy4c7@gmail.com>
2022-11-10 14:47:40 +0900

    prettierを導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Button, Box, Stack, LinearProgress } from '@mui/material';
-import useApi from './api/useApi';
+import React, { useState } from "react"
+import { Button, Box, Stack, LinearProgress } from "@mui/material"
+import useApi from "./api/useApi"
 
 function App() {
   const [file, setFile] = useState<File>()
@@ -14,7 +14,11 @@ function App() {
   return (
     <Stack>
       <Stack component="form" direction="row" spacing={2} onSubmit={onSubmit}>
-        <Stack direction="row" spacing={1} sx={{flex: "1 1 auto", border: '1px dashed grey' }}>
+        <Stack
+          direction="row"
+          spacing={1}
+          sx={{ flex: "1 1 auto", border: "1px dashed grey" }}
+        >
           <Button
             disabled={state.isPolling}
             variant="contained"
@@ -25,24 +29,41 @@ function App() {
               type="file"
               hidden
               onChange={(e) => {
-                if(state && e.target.files?.[0]) {
+                if (state && e.target.files?.[0]) {
                   setFile(e.target.files?.[0])
                 }
               }}
             />
           </Button>
-          <Stack direction="row" sx={{flex: "1 1 auto", alignItems: 'center'}}>
+          <Stack
+            direction="row"
+            sx={{ flex: "1 1 auto", alignItems: "center" }}
+          >
             {file?.name}
           </Stack>
         </Stack>
         <Box>
-          <Button disabled={state.isPolling} variant="contained" color='primary' type='submit'>送信</Button>
+          <Button
+            disabled={state.isPolling}
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            送信
+          </Button>
         </Box>
-      </Stack> 
-      <LinearProgress sx={{margin: '4px 0', visibility: state.isPolling ? 'visible' : 'hidden'}}/>
-      <Button variant="contained" disabled={!state.url} href={state.url}>ダウンロード</Button>
+      </Stack>
+      <LinearProgress
+        sx={{
+          margin: "4px 0",
+          visibility: state.isPolling ? "visible" : "hidden",
+        }}
+      />
+      <Button variant="contained" disabled={!state.url} href={state.url}>
+        ダウンロード
+      </Button>
     </Stack>
-  );
+  )
 }
 
 export default App
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Container, createTheme, ThemeProvider, CssBaseline } from '@mui/material';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import axios, { AxiosInstance } from 'axios';
-import { Provider } from 'inversify-react';
-import { Container as InversifyContainer } from 'inversify'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import {
+  Container,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@mui/material"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import axios, { AxiosInstance } from "axios"
+import { Provider } from "inversify-react"
+import { Container as InversifyContainer } from "inversify"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-const isDarkMode = matchMedia('(prefers-color-scheme: dark)').matches
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const isDarkMode = matchMedia("(prefers-color-scheme: dark)").matches
 const theme = createTheme({
   palette: {
-    mode: isDarkMode ? 'dark' : 'light'
-  }
+    mode: isDarkMode ? "dark" : "light",
+  },
 })
 
 root.render(
   <React.StrictMode>
-    <Provider container={() => {
-      const container = new InversifyContainer()
-      container.bind<AxiosInstance>('axios').toConstantValue(axios.create({
-        baseURL: process.env.REACT_APP_API_URL
-      }))
-      return container
-    }}>
+    <Provider
+      container={() => {
+        const container = new InversifyContainer()
+        container.bind<AxiosInstance>("axios").toConstantValue(
+          axios.create({
+            baseURL: process.env.REACT_APP_API_URL,
+          })
+        )
+        return container
+      }}
+    >
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Container maxWidth="md">
@@ -36,9 +43,9 @@ root.render(
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
